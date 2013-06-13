641141a24688f2125bce96c3ebf52279f6993993
unknown <ashan@.starsoft.mys>
2013-06-13 11:21:42 +0530

    commit

--- mobile_website/codiqa.ext.js
+++ mobile_website/codiqa.ext.js
@@ -5,11 +5,35 @@ $(document).ready(function () {
     $('#about').click(function () {
         window.location.href = "#page2";
     });
+    $('#accele').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#camera').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#geolocation').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#device').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#event').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#file').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#media').click(function () {
+        window.location.href = "#page2";
+    });
+    $('#contacts').click(function () {
+        window.location.href = "#page2";
+    });
 
 
     function exitAppPopup() {
-        navigator.notification.confirm('Exit PhoneGap' + device.cordova + 'Demo?',
-function (button) {
+        navigator.notification.confirm('Exit PhoneGap' + device.cordova + 'Demo?'
+        ,function (button) {
     if (button == 2) {
         navigator.app.exitApp();
     }
@@ -21,7 +45,7 @@ function (button) {
     }
 
 
-    document.addEventListener("back", function () {
+    document.addEventListener("backbutton", function () {
     
         if ($('.ui-page-active').attr('id') == 'page1') {
             alert(back);
